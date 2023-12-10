ca3fd8776c538ce578f9ed5efffee1acbd5f705e
Zal Solmuş <72752991+zalcod@users.noreply.github.com>
2023-12-10 11:10:19 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,68 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    <header>
+      <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+      <div class="wrapper">
+        <HelloWorld msg="You did it!" />
+      </div>
+    </header>
+
+    <main>
+      <TheWelcome />
+    </main>
+
+    <div>
+      <img :src="imageSrc" alt="Suple Evart" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+
+export default defineComponent({
+  data() {
+    return {
+      imageSrc: 'https://a0.muscache.com/im/pictures/73640504/9fefd9b9_original.jpg?im_w=720',
+    };
+  },
+});
+
+
+</script>
+
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
